469f20a35021a4b637e0ee466cc30b5b6aaf8854
proxyn68-hash <proxyn68@gmail.com>
2026-08-05 23:14:51 +0330

    Update shahrdari.js

--- shahrdari.js
+++ shahrdari.js
@@ -1,1 +1,38 @@
-let technical=72700,finalAmount=0;function selectFixed(){technical=72700;customTechnical.disabled=true;fixedCard.classList.add('active');customCard.classList.remove('active')}function selectCustom(){technical=0;customTechnical.disabled=false;customTechnical.focus();customCard.classList.add('active');fixedCard.classList.remove('active')}function calculate(){let outside=Number(outside.value),fr=Number(franchise.value),c=Number(customTechnical.value);if(!customTechnical.disabled)technical=c;finalAmount=Math.floor(outside*0.7+technical+fr);result.innerHTML=finalAmount.toLocaleString()+' تومان'}function copyResult(){navigator.clipboard.writeText(finalAmount.toString());toast.classList.add('show');setTimeout(()=>toast.classList.remove('show'),1500)}
+let technical = 727000, finalAmount = 0;
+
+function selectFixed() {
+    technical = 727000;
+    customTechnical.disabled = true;
+    fixedCard.classList.add('active');
+    customCard.classList.remove('active');
+}
+
+function selectCustom() {
+    technical = 0;
+    customTechnical.disabled = false;
+    customCard.classList.add('active');
+    fixedCard.classList.remove('active');
+}
+
+function calculate() {
+    let outside = Number(document.getElementById('outside').value);
+    let fr = Number(document.getElementById('franchise').value);
+    let c = Number(document.getElementById('customTechnical').value);
+
+    if (!customTechnical.disabled) {
+        technical = c;
+    }
+
+    finalAmount = Math.floor(outside * 0.7 + technical + fr);
+
+    result.innerHTML = finalAmount.toLocaleString() + ' ریال';
+}
+
+function copyResult() {
+    navigator.clipboard.writeText(finalAmount.toString());
+    toast.classList.add('show');
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 1500);
+}
