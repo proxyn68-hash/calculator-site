f568837b774b42e6a8a31eae01e6eec917fc9d75
proxyn68-hash <proxyn68@gmail.com>
2026-08-06 00:21:14 +0330

    Update shahrdari.js

--- shahrdari.js
+++ shahrdari.js
@@ -15,20 +15,51 @@ formatNumberInput(outside);
 formatNumberInput(franchise);
 formatNumberInput(customTechnical);
 
+
 function getNumber(value) {
     return Number(String(value).replace(/,/g, '')) || 0;
 }
 
+
+// حرکت با Enter بین فیلدها
+const inputs = [outside, franchise, customTechnical];
+
+inputs.forEach((input, index) => {
+    input.addEventListener('keydown', function (e) {
+
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            for (let i = index + 1; i < inputs.length; i++) {
+
+                if (!inputs[i].disabled) {
+                    inputs[i].focus();
+                    return;
+                }
+
+            }
+
+            calculate();
+        }
+
+    });
+});
+
+
 function selectFixed() {
+
     technical = 727000;
 
     customTechnical.disabled = true;
 
     fixedCard.classList.add('active');
     customCard.classList.remove('active');
+
 }
 
+
 function selectCustom() {
+
     technical = 0;
 
     customTechnical.disabled = false;
@@ -36,16 +67,21 @@ function selectCustom() {
 
     customCard.classList.add('active');
     fixedCard.classList.remove('active');
+
 }
 
+
 function calculate() {
+
     let outsideValue = getNumber(outside.value);
     let fr = getNumber(franchise.value);
     let c = getNumber(customTechnical.value);
 
+
     if (!customTechnical.disabled) {
         technical = c;
     }
+
 
     finalAmount = Math.floor(
         outsideValue * 0.7 +
@@ -53,15 +89,24 @@ function calculate() {
         fr
     );
 
-    result.innerHTML = finalAmount.toLocaleString('en-US') + ' ریال';
+
+    result.innerHTML =
+        finalAmount.toLocaleString('en-US') + ' ریال';
+
 }
 
+
 function copyResult() {
+
     navigator.clipboard.writeText(finalAmount.toString());
 
     toast.classList.add('show');
 
+
     setTimeout(() => {
+
         toast.classList.remove('show');
+
     }, 1500);
+
 }
